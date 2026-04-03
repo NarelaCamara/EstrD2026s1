e3c7e635049a97b7b11ae8e7130f3ca8292f702c
RoxanaCamara <roxana.camara07@gmail.com>
2026-04-03 18:47:44 -0300

    feat: tareas

--- tp-1/tp-1.hs
+++ tp-1/tp-1.hs
@@ -2,7 +2,6 @@
 
 {-- 1. --}
 
-
 {--
     Devuelve numeros positivos, no devuelve numeros negativos, ni con coma. 
     PRECOND: number tiene que ser igual o mayor a cero.
@@ -72,3 +71,91 @@ diezEjemplo4 = maxDelPar ((maxDelPar (divisionYResto (sumar 4 6) (sucesor 6))), 
 
 {- TIPOS ENUMERATIVOS -}
 {-- 3. --}
+
+data Dir = Norte | Sur | Este | Oeste 
+
+{-- a. --}
+
+
+opuesto :: Dir -> Dir
+opuesto Norte = Sur
+opuesto Sur = Norte
+opuesto Este = Oeste
+opuesto Oeste = Este
+
+
+{-- b. --}
+
+sonIguales :: Dir -> Dir -> Bool
+sonIguales Norte Norte = True
+sonIguales Sur Sur = True
+sonIguales Este Este = True
+sonIguales Oeste Oeste = True
+sonIguales _ _ = False
+
+
+{-- c. --}
+
+{--
+    Devuelve la dirección siguiente a la dada, en el sentido de las agujas del reloj.
+    PRECOND: La dirección dada debe ser una de las 3 direcciones (norte, este y sur).  
+    Como no existe Oeste. Es una funcion parcial y no se puede utilizar a Oeste.   
+--}
+siguiente :: Dir -> Dir
+siguiente Norte = Este
+siguiente Este = Sur
+siguiente Sur = Norte
+
+data DiaDeSemana = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo 
+
+{-- 2. --}
+
+{-- a. --}
+{--
+    Devuelve el siguiente día de la semana al dado.
+    PRECOND: El día dado debe ser uno de los 6 días (Lunes, Martes, Miercoles, Jueves, Viernes y Sabado).  
+    Como no existe Domingo. Es una funcion parcial y no se puede utilizar a Domingo.
+--}
+
+primeroYUltimoDia :: (DiaDeSemana ,DiaDeSemana)
+primeroYUltimoDia = ( Lunes , Domingo)
+
+
+{-- b. --}
+empiezaConM :: DiaDeSemana -> Bool
+empiezaConM Martes = True
+empiezaConM Miercoles = True
+empiezaConM _ = False 
+
+{-- c.    --}
+
+sonIgualesDias :: DiaDeSemana -> DiaDeSemana -> Bool
+sonIgualesDias Lunes Lunes = True
+sonIgualesDias Martes Martes = True 
+sonIgualesDias Miercoles Miercoles = True
+sonIgualesDias Jueves Jueves = True
+sonIgualesDias Viernes Viernes = True
+sonIgualesDias Sabado Sabado = True
+sonIgualesDias Domingo Domingo = True
+sonIgualesDias _ _ = False
+
+
+vieneDespues :: DiaDeSemana -> DiaDeSemana -> Bool
+vieneDespues day1 day2 = 
+    if sonIgualesDias day1 Domingo then False
+    else if sonIgualesDias day1 Martes &&  sonIgualesDias day2 Lunes then False
+    else if sonIgualesDias day1 Miercoles && ( sonIgualesDias day2 Martes || sonIgualesDias day2 Lunes )then False
+    else if sonIgualesDias day1 Jueves && ( sonIgualesDias day2 Miercoles || sonIgualesDias day2 Martes || sonIgualesDias day2 Lunes) then False
+    else if sonIgualesDias day1 Viernes && ( sonIgualesDias day2 Jueves ||  sonIgualesDias day2 Miercoles || sonIgualesDias day2 Martes || sonIgualesDias day2 Lunes) then False
+    else if sonIgualesDias day1 Sabado && ( sonIgualesDias day2 Viernes  || sonIgualesDias day2 Jueves ||  sonIgualesDias day2 Miercoles || sonIgualesDias day2 Martes || sonIgualesDias day2 Lunes) then False
+    else True
+    
+
+{-- d.  --}
+
+estaEnMedio :: DiaDeSemana -> Bool
+estaEnMedio Lunes = False
+estaEnMedio Domingo = False
+estaEnMedio _ = True
+
+
