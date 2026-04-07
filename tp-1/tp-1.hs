bf1d0bae4f3e19fbbbff351273ea7c3290a1b29e
RoxanaCamara <roxana.camara07@gmail.com>
2026-04-07 06:54:48 -0300

    feat: correcion

--- tp-1/tp-1.hs
+++ tp-1/tp-1.hs
@@ -356,9 +356,8 @@ laQueEsMayor (PersonaC name1 age1) (PersonaC name2 age2)  = if esMayorQueLaOtra 
 {--  2. --}
 data TipoDePokemon = Agua | Fuego | Planta deriving (Show, Eq)
 
-data Pokemon = PokemonC  TipoDePokemon  Int deriving (Show, Eq)
-data Entrenador = EntrenadorC String  Pokemon  Pokemon deriving (Show, Eq)
-
+data Pokemon = PokemonC  TipoDePokemon  Int deriving Show
+data Entrenador = EntrenadorC String  Pokemon  Pokemon deriving Show
 
 {-- a. --}
 
