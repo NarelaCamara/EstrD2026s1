465aeed80d7080ea1c554c4bd32449499447aec6
NarelaCamara <roxana.camara07@gmail.com>
2026-05-12 14:29:07 -0300

    feat: map v2

--- tp-6/MapV2.hs
+++ tp-6/MapV2.hs
@@ -1,19 +1,19 @@
-module Map (
-    Map,
+module MapV2 (
+    MapV2,
     emptyM,
     assocM,
     lookupM,
     deleteM,
     keys,
-    mapEjemplo1,
-    mapEjemplo2,
-    mapEjemplo3
+    mapV2Ejemplo1,
+    mapV2Ejemplo2,
+    mapV2Ejemplo3
 )
 where
-data Map k v = M [(k,v)] deriving Show
+data MapV2 k v = M [(k,v)] deriving Show
 
 {-
-    INVARIANTE DE REPRENSENTACION:  lista de pares-clave valor con claves rep etidas 
+    INVARIANTE DE REPRENSENTACION:  lista de pares-clave valor con claves repetidas 
 -}
 
 
@@ -22,34 +22,31 @@ data Map k v = M [(k,v)] deriving Show
     PRECONDICION:   
     COSTO:   O(1) constante
 -}
-emptyM :: Map k v
+emptyM :: MapV2 k v
 emptyM = (M [])
 
 
 {-
     Proposito: 
     PRECONDICION:   
-    COSTO:   O(n) Lineal
+    COSTO:    O(1) Constante
 -}
-assocM :: Eq k => k -> v -> Map k v -> Map k v
-assocM c1 v1 (M xs) =  (M (associar c1 v1 xs))
+assocM :: Eq k => k -> v -> MapV2 k v -> MapV2 k v
+assocM c1 v1 (M xs) =  (M ((c1,v1):xs))
 
 {-
     Proposito: 
     PRECONDICION:   
     COSTO:   O(1) constante +  O(1) constante +  O(n) Lineal
 -}
-associar :: Eq k => k -> v -> [(k, v)] ->[(k, v)] 
-associar c1 v1 [] = [(c1, v1)]
-associar c1 v1 ((c2,v2):xs) = if c1 == c2 then (c1, v1) : xs else (c2,v2) : associar c1 v1 xs
 
 
 {-
     Proposito: 
-    PRECONDICION:   
+    PRECONDICION:   El primer elemento que cumpla la condicion, el mas reciente trae
     COSTO:   O(n) Lineal
 -}
-lookupM :: Eq k => k -> Map k v -> Maybe v
+lookupM :: Eq k => k -> MapV2 k v -> Maybe v
 lookupM c (M xs) = buscar c xs 
 
 {-
@@ -68,7 +65,7 @@ buscar c ((c1,v2):xs) = if c1 == c then (Just v2) else buscar c xs
     COSTO:   O(n) Lineal
 -}
 
-deleteM :: Eq k => k -> Map k v -> Map k v
+deleteM :: Eq k => k -> MapV2 k v -> MapV2 k v
 deleteM c1 (M xs) = (M (borrar c1 xs))
 
 {-
@@ -78,14 +75,14 @@ deleteM c1 (M xs) = (M (borrar c1 xs))
 -}
 borrar :: Eq k => k -> [(k,v)] -> [(k,v)]
 borrar c1 [] = []
-borrar c1 ((c2,v2):xs) = if c1 == c2 then xs else (c2,v2) : borrar c1 xs
+borrar c1 ((c2,v2):xs) = if c1 == c2 then borrar c1 xs else (c2,v2) : borrar c1 xs
 
 {-
     Proposito: 
     PRECONDICION:   
     COSTO:   O(n) Lineal
 -}
-keys :: Map k v -> [k]
+keys :: MapV2 k v -> [k]
 keys (M []) = []
 keys (M xs) = obtenerClaves xs
 
@@ -93,17 +90,26 @@ keys (M xs) = obtenerClaves xs
 {-
     Proposito: 
     PRECONDICION:   
-    COSTO:   O(n) Lineal
+    COSTO:   O(n) Lineal *  O(n) Lineal =  O(n^2) Cuadratico
 -}
 obtenerClaves:: [(k,v)] -> [k]
 obtenerClaves [] = []
-obtenerClaves ((c1,v1):xs) = c1 : obtenerClaves xs
+obtenerClaves ((c1,v1):xs) = if existIn c1 xs then  obtenerClaves xs else  c1 : obtenerClaves xs
 
 
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:   O(n) Lineal +  O(1) Constante
+-}
+existIn:: Eq k => k -> [(k,v)] -> Bool
+existIn c [] = False
+existIn c ((c1,v1):xs) = c1 == c || existIn c xs
 
-mapEjemplo1 = (M [(1, "Valor 1"), (2, "Valor 2"),(3, "Valor 3")])
-mapEjemplo3 = (M [(1, "Valor 666"), (2, "Valor 8"),(4, "Valor 4")])
+
+mapV2Ejemplo1 = (M [(1, "Valor 1"),(1, "Valor 667"), (2, "Valor 2"),(3, "Valor 3")])
+mapV2Ejemplo3 = (M [(1, "Valor 666"),(1, "Valor 1"), (2, "Valor 8"),(4, "Valor 4")])
 
 
-mapEjemplo2 :: Map String Int
-mapEjemplo2 = (M [("Clave 1", 1), ( "Clave 2", 2),("Clave 3", 3)])
+mapV2Ejemplo2 :: MapV2 String Int
+mapV2Ejemplo2 = (M [("Clave 1", 1),("Clave 1", 666), ( "Clave 2", 2),("Clave 3", 3)])
