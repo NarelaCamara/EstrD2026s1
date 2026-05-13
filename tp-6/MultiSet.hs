f417006952267ef34a398d07ea0096faf4d17513
RoxanaCamara <roxana.camara07@gmail.com>
2026-05-12 22:50:20 -0300

    feat: ms

--- tp-6/MultiSet.hs
+++ tp-6/MultiSet.hs
@@ -1,3 +1,5 @@
+import Map
+
 module MultiSet (
     MultiSet,
     emptyMS,
@@ -11,4 +13,45 @@ module MultiSet (
     multiSetEjemplo3
 )
 where
-data MultiSet a = MS a deriving Show
+data MultiSet a = MS (Map a Int) deriving Show
+
+{-
+    INVARIANTE DE REPRENSENTACION:  
+-}
+
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:   O(1) Constante
+-}
+emptyMS :: MultiSet a
+emptyMS = (MS emptyM)
+
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:  Map V1 >>  O(n) Lineal
+-}
+addMS :: Ord a => a -> MultiSet a -> MultiSet a
+addMS n (MS map) = assocM  n (fromJust''(lookupM n map))
+
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:   O(1) Constante
+-}
+
+fromJust'':: Maybe Int -> Int
+fromJust'' (Just e) = e
+fromJust'' Nothing = 0
+
+
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:  
+-}
+ocurrencesMS :: Ord a => a -> MultiSet a -> Int
+ocurrencesMS n (MS map) = 
+
+
