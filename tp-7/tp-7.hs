d9cf91e4b92b0c1f0dfbdbbbd75a909db958f367
NarelaCamara <roxana.camara07@gmail.com>
2026-05-16 01:27:23 -0300

    feat: deleteBST

--- tp-7/tp-7.hs
+++ tp-7/tp-7.hs
@@ -63,5 +63,39 @@ ejemploTreeBST = NodeT 12 (NodeT 8 EmptyT EmptyT) (NodeT 18 EmptyT EmptyT)
 
 Porque este ejemplo no es un arbol bST, Ya que en los bst el root debe ser el menor de los nodos, y en este caso el root es 12, pero el nodo 8 es menor que el root, entonces no cumple con la propiedad de los bst.--}
 
-ejemploTreeBST = NodeT 12 (NodeT 8 EmptyT EmptyT) (NodeT 18 EmptyT EmptyT)
+
+{--Ejemplo de uso --}
+ejemploTreeBST = NodeT 12 (NodeT 8 (NodeT 4 EmptyT EmptyT) (NodeT 10 EmptyT EmptyT)) (NodeT 18 (NodeT 16 EmptyT EmptyT) (NodeT 25 EmptyT EmptyT))
 belongsBSTEjemplo = belongsBST 19 ejemploTreeBST
+
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:   O(1) Constante +  O(1) Constante +  O(1) Constante +  O(log n) logatirmo =  O(log n) logatirmo
+-}
+
+insertBST :: Ord a => a -> Tree a -> Tree a
+insertBST e EmptyT = (NodeT e EmptyT EmptyT)
+insertBST e (NodeT n t1 t2) = 
+    if (e == n) then (NodeT n t1 t2) else (if (e < n) then (NodeT n (insertBST e t1) t2) else (NodeT n t1 (insertBST e t2)))
+
+{--Ejemplo de uso --}
+insertBSTEjemplo = insertBST 2 (insertBST 14 ejemploTreeBST)
+
+
+{-
+    Proposito: 
+    PRECONDICION:   
+    COSTO:  
+-}
+deleteBST :: Ord a => a -> Tree a -> Tree a
+deleteBST e EmptyT = EmptyT
+deleteBST e (NodeT n t1 t2) = if (e == n) then (refactor t1 t2) else (if (e < n) then (NodeT n (deleteBST e t1) t2) else (NodeT n t1 (deleteBST e t2)))
+
+refactor :: Tree a -> Tree a -> Tree a
+refactor EmptyT t2 = t2
+refactor t1 EmptyT = t1 
+refactor (NodeT e t11 t12) t2 = (NodeT e (refactor t11 t12) t2)
+
+{--Ejemplo de uso --}
+deleteBSTEjemplo = deleteBST 18 ejemploTreeBST
