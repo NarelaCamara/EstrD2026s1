2a1e2ef75592dc4c5dec91ddaf66e252e2a70741
NarelaCamara <roxana.camara07@gmail.com>
2026-06-12 19:55:51 -0300

    feat: grafico costos

--- tp-10/Persona.cpp
+++ tp-10/Persona.cpp
@@ -2,12 +2,9 @@
 #include "Persona.h"
 using namespace std;
 
-/**struct Persona
-{
-    string nombre;
-    int edad;
-}; */
 
+/* Costo: Un frame, constante, utiliza 3 campos en memoria stack y 2 en heap */
+// Devuelve a una p ersona nueva, con el nombre y la edad dados
 Persona consPersona(string nombre, int edad)
 {
     PersonaSt* p = new PersonaSt;
@@ -15,34 +12,147 @@ Persona consPersona(string nombre, int edad)
     p->edad = edad;
     return p;
 };
-// Devuelve a una p ersona nueva, con el nombre y la edad dados
+/*
+STACK (consPersona activa)
++-----------------------------------------------------+
+| consPersona frame                                   |         |
+|                                                     |
+| p : PersonaSt                                       |
+|   - nombre                                          |
+|   - edad                                            |
+|   - p : PersonaSt --> HEAP {nombre,edad}            |                                               |
++-----------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| p: PersonaSt { nombre, edad }                       |
++-----------------------------------------------------+ 
+*/
+
+// Devuelve el nombre de una p ersona
+/* Costo: Un frame, constante */
 string nombre(Persona p) {
     return p->nombre;
 };
-// Devuelve el nombre de una p ersona
 
+/*
+STACK
++-----------------------------------------------------+
+| nombre frame                                   |         |
+|                                                     |
+| p : nombre                                       |
+|   - p : PersonaSt --> HEAP {nombre,edad}            |                                               |
++-----------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+| p: PersonaSt { nombre, edad }                       |
++-----------------------------------------------------+ 
+*/
+
+
+// Devuelve la edad de una p ersona
+/* Costo: Un frame, constante */
 int edad(Persona p){
     return p->edad;
 };
-// Devuelve la edad de una p ersona
+/*
+STACK
++-----------------------------------------------------+
+| edad frame                                   |         |
+|                                                     |
+|   - p : PersonaSt --> HEAP {nombre,edad}            |                                               |
++-----------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| p: PersonaSt { nombre, edad }                       |
++-----------------------------------------------------+ 
+*/
+
+
+/* Costo: Un frame, constante */
+// Aumenta en uno la edad de la p ersona.
 void crecer(Persona p){
     p->edad++;
 };
-// Aumenta en uno la edad de la p ersona.
+/*
+STACK
++-----------------------------------------------------+
+| crecer frame                                   |         |
+|                                                     |
+|   - p : PersonaSt --> HEAP {nombre,edad}            |                                               |
++-----------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| p: PersonaSt { nombre, edad }                       |
++-----------------------------------------------------+ 
+*/
+
+
+
+/* Costo: Un frame, constante */
+// Modifica el nombre una p ersona.
 void cambioDeNombre(string nombre, Persona p){
     p->nombre = nombre;
 };
-// Modifica el nombre una p ersona.
+/*
+STACK
++-----------------------------------------------------+
+| cambioDeNombre frame                                   |         |
+|                                                     |
+|   - p : PersonaSt --> HEAP {nombre,edad}
+    - n : nombre -> string
+|                                               |
++-----------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| p: PersonaSt { nombre, edad }                       |
++-----------------------------------------------------+ 
+*/
+
+/* Costo: Un frame, constante */
+// Dadas dos p ersonas indica si la primera es mayor que la segunda.
 bool esMayorQueLaOtra(Persona p1, Persona p2){
     return p1->edad > p2->edad;
 };
-// Dadas dos p ersonas indica si la primera es mayor que la segunda.
+/*
+STACK
++-----------------------------------------------------+
+| cambioDeNombre frame                                |         
+|                                                     |
+|   - p1 : PersonaSt --> HEAP {nombre,edad}           |
+|   - p2 : PersonaSt --> HEAP {nombre,edad}           |
+|                                                     |
++-----------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| p1: PersonaSt { nombre, edad }                      |
+|  p2: PersonaSt { nombre, edad }                     |
++-----------------------------------------------------+ 
+*/
+
+
+// Dadas dos p ersonas devuelve a la p ersona que sea mayor
 Persona laQueEsMayor(Persona p1, Persona p2){
     return p1->edad > p2->edad ? p1 : p2;
 };
-// Dadas dos p ersonas devuelve a la p ersona que sea mayor
+/*
+STACK
++-----------------------------------------------------+
+| laQueEsMayor frame                                  |         
+|                                                     |
+|   - p1 : PersonaSt --> HEAP {nombre,edad}           |
+|   - p2 : PersonaSt --> HEAP {nombre,edad}           |
+|                                                     |
++-----------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+|  p1: PersonaSt { nombre, edad }                     |
+|  p2: PersonaSt { nombre, edad }                     |
++-----------------------------------------------------+ 
+*/
--- tp-10/Pokemon.cpp
+++ tp-10/Pokemon.cpp
@@ -2,6 +2,7 @@
 #include "Pokemon.h"
 using namespace std;
 
+/*costo es constante, un frame*/
 // Dado un tipo devuelve un pokémon con 100 % de energía.
 Pokemon consPokemon(TipoDePokemon tipo)
 {
@@ -10,18 +11,65 @@ Pokemon consPokemon(TipoDePokemon tipo)
     p->vida = 100;
     return p;
 };
+/*
+STACK
++-----------------------------------------------------+
+| consPokemon frame                                  |         
+|                                                     |
+|   - p : PokeSt --> HEAP {tipo, vida}   
+    - tipo: string
+|                                                     |
++-----------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+|  p: PokeSt { tipo, vida }                     |
++-----------------------------------------------------+ 
+*/
+
+/*costo es constante, un frame*/
 // Devuelve el tip o de un p okémon.
 TipoDePokemon tipoDePokemon(Pokemon p)
 {
     return p->tipo;
 };
 
+/*
+STACK
++-----------------------------------------------------+
+| consPokemon frame                                  |         
+|                                                     |
+|   - p : PokeSt --> HEAP {tipo, vida}   
+|                                                     |
++-----------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+|  p: PokeSt { tipo, vida }                     |
++-----------------------------------------------------+ 
+*/
+
+
 // Devuelve el porcenta je de energía.
 int energia(Pokemon p)
 {
     return p->vida;
 };
+/*
+STACK
++-----------------------------------------------------+
+| consPokemon frame                                   |         
+|                                                     |
+|   - p : PokeSt --> HEAP {tipo, vida}                |  
+|                                                     |
++-----------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+|  p: PokeSt { tipo, vida }                     |
++-----------------------------------------------------+ 
+*/
+
 
 // Le resta energía al p okémon.
 void perderEnergia(int energia, Pokemon p)
@@ -29,6 +77,22 @@ void perderEnergia(int energia, Pokemon p)
     int newEnergia = p->vida - energia;
     p->vida = newEnergia > 0 ? newEnergia : 0;
 };
+/*
+STACK
++-----------------------------------------------------+
+| perderEnergia frame                                 |
+|   - p : PokeSt --> HEAP {tipo, vida}                |
+|   - energia -- int  
+    - newEnergia -- int                               |
++-----------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+|  p: PokeSt { tipo, vida }                           |
++-----------------------------------------------------+ 
+*/
+
+
 
 /*Dados dos pokémon indica si el primero, en base al tipo, es sup erior al segundo. Agua sup era
 a fuego, fuego a planta y planta a agua. Y cualquier otro caso es falso.
@@ -37,3 +101,17 @@ bool superaA(Pokemon p1, Pokemon p2)
 {
     return (p1->tipo == "AGUA" && p2->tipo == "FUEGO") || (p1->tipo == "FUEGO" && p2->tipo == "PLANTA") || (p1->tipo == "PLANTA" && p2->tipo == "AGUA");
 };
+/*
+STACK
++-----------------------------------------------------+
+| perderEnergia frame                                 |
+|   - p1 : PokeSt --> HEAP {tipo, vida}                |
+|   - p2 : PokeSt --> HEAP {tipo, vida}                |
++-----------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+|  p1: PokeSt { tipo, vida }                           |
+|  p2: PokeSt { tipo, vida }                           |
++-----------------------------------------------------+ 
+*/
