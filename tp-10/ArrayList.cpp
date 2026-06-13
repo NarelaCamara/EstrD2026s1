2a0038b5d585dfd82acd7066f7043cf07e72a9da
NarelaCamara <roxana.camara07@gmail.com>
2026-06-13 04:37:07 -0300

    feat: arraylist

--- tp-10/ArrayList.cpp
+++ tp-10/ArrayList.cpp
@@ -6,59 +6,133 @@
  * siempre se va a contar desde el 1 y no desde el cero
  */
 
+
+/**
+ * costo: constante, un frame, solo prepara para guardar 15 espacios de memoria
+ */
 // Crea una lista con 0 elementos.
 ArrayList newArrayList()
 {
     ArrayList l = new ArrayListSt;
     l->cantidad = 0;
     l->capacidad = 16;
-    l->elementos = new int[16-1];
+    l->elementos = new int[16];
     return l;
 };
 /*
 STACK
-+-----------------------------------------------------+
-| consPokemon frame                                  |         
-|                                                     |
-|   - p : PokeSt --> HEAP {tipo, vida}   
-    - tipo: string
-|                                                     |
-+-----------------------------------------------------+
++---------------------------------------------------------------------------+
+| consPokemon frame                                                         |         
+|   - l : ArrayList --> HEAP { cantidad, capacidad, elementos--> HEAP int  } |
++---------------------------------------------------------------------------+
 
 HEAP
 +-----------------------------------------------------+
-|  p: PokeSt { tipo, vida }                     |
+| - cantidad: int                                     |
+| - capacidad: int                                    |
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [16-1] int                             |
 +-----------------------------------------------------+ 
 */
 
+/** costo: constante, utiliza un frame */
 // Crea una lista con 0 elementos y una capacidad dada por parámetro.
 ArrayList newArrayListWith(int capacidad)
 {
     ArrayList l = new ArrayListSt;
     l->capacidad = capacidad;
     l->cantidad = 0;
-    l->elementos = new int[capacidad-1];
+    l->elementos = new int[capacidad];
     return l;
 };
 
+/*
+STACK
++---------------------------------------------------------------------------+
+| consPokemon frame                                                         |         
+|   - l : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  } |
++---------------------------------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [capacidad] int                        |
++-----------------------------------------------------+ 
+*/
+
+
+/** costo: constante, utiliza un frame */
 // Devuelve la cantidad de elementos existentes.
 int lengthAL(ArrayList xs)
 {
-    return xs->cantidad;//wtffffffffff
+    return xs->cantidad;
 };
+/*
+STACK
++---------------------------------------------------------------------------+
+| consPokemon frame                                                         |         
+|   - xs : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  }|
++---------------------------------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [cantidad] int                         |
++-----------------------------------------------------+ 
+*/ 
+
+// RAROOO SERIA CANTIDAD O CAPACIDAD EN LA HEAP?
+
+
+/** costo: constante, utiliza un frame */
 // Devuelve el iésimo elemento de la lista.
 int get(int i, ArrayList xs)
 {
-    return xs->elementos[i - 1];
+    return xs->elementos[i];
 };
+/*
+STACK
++---------------------------------------------------------------------------+
+| consPokemon frame                                                         |         
+|   - xs : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  }|
+    - i int                                                                 |    
++---------------------------------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [cantidad] int                         |
++-----------------------------------------------------+ 
+*/
+
+/** costo: constante, utiliza un frame */
 // Reemplaza el iésimo elemento por otro dado.
 void set(int i, int x, ArrayList xs)
 {
-    xs->elementos[i - 1] = x;
+    xs->elementos[i] = x;
 };
+/*
+STACK
++---------------------------------------------------------------------------+
+| consPokemon frame                                                         |         
+|   - xs : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  }|
+    - i int                                                                 |   
+    - x int                                                                 |    
++---------------------------------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [cantidad] int                         |
++-----------------------------------------------------+ 
+*/
+
+/* */
 // Decrementa o aumenta la capacidad del array.
 // Nota: en caso de decrementarla, se pierden los elementos del final de la lista.
 void resize(int capacidad, ArrayList xs)
@@ -75,6 +149,26 @@ void resize(int capacidad, ArrayList xs)
     xs->capacidad = capacidad;
     xs->cantidad = lenght;
 };
+/*
+STACK
++---------------------------------------------------------------------------+
+| resize frame                                                              |         
+|   - xs : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  }|
+|   - capacidad int                                                         |   
+|   - lenght int                                                            |  
+|   - nuevosElementos  --> HEAP int                                         |
+|    - i int                                                                |  
++---------------------------------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [cantidad] int                         |
++-----------------------------------------------------+ 
+*/
+
+
 
 // Agrega un elemento al final de la lista.
 void add(int x, ArrayList xs)
@@ -96,7 +190,26 @@ void add(int x, ArrayList xs)
     xs->elementos[xs->cantidad] = x;
     xs->cantidad++;
 };
+/*
+STACK
++---------------------------------------------------------------------------+
+| resize frame                                                              |         
+|   - xs : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  }|
+|   - capacidad int                                                         |   
+|   - lenght int                                                            |  
+|   - nuevosElementos  --> HEAP int                                         |
+|    - i int                                                                |  
++---------------------------------------------------------------------------+
 
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [cantidad] int                         |
++-----------------------------------------------------+ 
+*/
+
+/* */
 // Borra el último elemento de la lista.
 void remove(ArrayList xs)
 {
@@ -115,3 +228,21 @@ void remove(ArrayList xs)
         xs->cantidad = newCantidad;
     }
 };
+/*
+STACK
++---------------------------------------------------------------------------+
+| remove frame                                                              |         
+|   - xs : ArrayList --> HEAP {cantidad, capacidad, elementos--> HEAP int  }|
+|   - newCantidad int                                                         |   
+|   - lenght int                                                            |  
+|   - nuevosElementos  --> HEAP int                                         |
+|    - i int                                                                |  
++---------------------------------------------------------------------------+
+
+HEAP
++-----------------------------------------------------+
+| - elementos: [0] int                                |
+| - elementos: [...] int                              |
+| - elementos: [cantidad-1] int                         |
++-----------------------------------------------------+ 
+*/
