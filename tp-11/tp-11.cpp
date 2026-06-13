446851a581b0a9a24d637238abdee45b752403ec
NarelaCamara <roxana.camara07@gmail.com>
2026-06-13 06:27:38 -0300

    feat: main

--- tp-11/tp-11.cpp
+++ tp-11/tp-11.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 //cero es falso y uno es verdadero
 int main() {
+    cout << "LinkedList ----------------------------------------- " << endl;
     LinkedList l = nil();
     Cons(1,l);
     Cons(2,l);
@@ -15,26 +16,26 @@ int main() {
     showList(l);
     cout << "Second: " << head(l) << endl;
     cout << "isEmpty: " << (isEmpty(l) ? "true" : "false") << endl;
-    Snoc(5,l);
+    Snoc(5,l); //falto 5 donde esta???
     Snoc(6,l);
-    cout << "length: " << length(l)<< endl;
+    cout << "length: " << length(l)<< endl; //deberia ser 5
     showList(l);
+
+    cout << "ListIterator ----------------------------------------- " << endl;
     ListIterator li = getIterator(l);
     cout << "getIterator: ";
     showIterator(li);
     cout << "current: " << current(li) << endl;
     SetCurrent(666, li);
     cout << "setCurrent: " << current(li) << endl;
+    showIterator(li);
     Next(li);
     cout << "setCurrent: " << current(li) << endl;
-    cout << "atEnd: " << atEnd(li) << endl;
-
+    showIterator(li);
+    cout << "atEnd: " << (atEnd(li) ? "true" : "false")<< endl;
     DisposeIterator(li);
     DestroyL(l);
 
-    cout << "Se borraron DisposeIterator y DestroyL: ";
-     cout << "DisposeIterator y DestroyL llamados correctamente." << endl;
-    cout << "l es " << l << endl;
-    cout << "li es " << li << endl;
+    cout << "DisposeIterator y DestroyL llamados correctamente." << endl;
     return 0;
 };
