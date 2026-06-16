bb8be99393ea61862332da80e0b797097718ac46
NarelaCamara <roxana.camara07@gmail.com>
2026-06-16 19:24:39 -0300

    feat: correccion

--- tp-11/Tree.h
+++ tp-11/Tree.h
@@ -41,6 +41,6 @@ ArrayList leaves(Tree t);
 //Dado un árb ol devuelve los elementos que se encuentran en sus ho jas.
  ArrayList levelN(int n, Tree t);
 //Dados un número n y un árb ol devuelve una lista con los no dos de nivel n
-void showTree(Tree t);
+void ShowTree(Tree t);
 
 #endif
