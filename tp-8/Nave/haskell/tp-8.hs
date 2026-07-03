f7999154ec4138990f07c53399acbcf6be390483
NarelaCamara <roxana.camara07@gmail.com>
2026-07-03 01:31:47 -0300

    feat: sectores

--- tp-8/Nave/haskell/tp-8.hs
+++ tp-8/Nave/haskell/tp-8.hs
@@ -5,7 +5,8 @@ import Nave
 {-
     Proposito: 
     PRECONDICION:   
-    COSTO:  
+    COSTO:  O(S log S) 
+        - siendo allSectors O(S log S)
 -}
 sectores:: Nave -> Set SectorId
 sectores n = allSectors (tripulantesN n) n
@@ -13,9 +14,16 @@ sectores n = allSectors (tripulantesN n) n
 {-
     Proposito: 
     PRECONDICION:   
-    COSTO:  
+    COSTO:  O(s log s + t) siendo s la cantiad de sectores y siento T la cantiidad de Tripulantes de la lista
+        - emptyS tiene un costo O(1)
+        - unionS tiene un costo O(s log s) siendo s la cantidad de elementos del set
+        - sectoresT tiene un costo O(1)
 -}
 
 allSectors:: [Tripulante] -> Nave -> Set SectorId
 allSectors [] n = emptyS
 allSectors (t:ts) n = unionS (sectoresT t) (allSectors ts n)  
+
+
+
+
