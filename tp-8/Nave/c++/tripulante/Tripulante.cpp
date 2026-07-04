153aab8ea8ebbf881b5239e66e896056130a449a
NarelaCamara <roxana.camara07@gmail.com>
2026-07-04 18:28:26 -0300

    feat: tripulante

---
+++ tp-8/Nave/c++/tripulante/Tripulante.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include "Tripulante.h"
+
+Tripulante creatT (Nombre n, Rango r){
+    TripulanteSt* t = new  TripulanteSt;
+    t->sectores = emptyS();
+    t->nombre= n;
+    t->rango= r;
+    return t;
+}
+
+void asignarS (SectorId s, Tripulante t){
+    addS(s, t->sectores);
+};
+
+Set SectoresT (Tripulante t){
+    return t->sectores;
+};
+
+Nombre nombre (Tripulante t){
+    return t->nombre;
+};
+
+Rango rango (Tripulante t){
+    return t->rango;
+};
---
+++ tp-8/Nave/c++/tripulante/Tripulante.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include "../set/Set.h"
+
+typedef std::string Nombre;
+typedef std::string SectorId;
+
+struct TripulanteSt {
+    Set sectores;
+    Nombre nombre;
+    Rango rango
+}
+
+Tripulante creatT (Nombre n, Rango r);
+
+void asignarS (SectorId s, Tripulante t);
+
+Set SectoresT (Tripulante t);
+
+Nombre nombre (Tripulante t);
+
+Rango rango (Tripulante t);
