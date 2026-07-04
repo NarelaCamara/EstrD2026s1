4c9bbec07b17a27e6c524755a21a16c680823c99
NarelaCamara <roxana.camara07@gmail.com>
2026-07-04 13:07:38 -0300

    feat: map

--- tp-8/Nave/c++/Map.cpp
+++ tp-8/Nave/c++/Map.cpp
@@ -2,12 +2,98 @@
 #include "Map.h"
 using namespace std;
 
-Map emptyM (); 
 
-void assocM (String k, String v, Map m);
 
-Maybe lookupM(String k, Map m);
+//emptyM :: Map k v O(1) --haskell
+//costo: constante O(1) 
+Map emptyM () {
+    MapSt* m = new MapSt;
+    m->cantidad=0;
+    m->primero=NULL;
+    return m;
+};
 
-void deleteM (String k, Map m); 
+//assocM :: k -> v -> Map k v -> Map k v O(log K) --haskell
+//costo: O (log K) donde K es la cantidad de las claves en el map - esta bien?
+void assocM (String k, String v, Map m){
+    int c = 0;
+    NodoS* tmp = map->primero;
 
-int* domM(Map m);
+    while(c < m->cantidad){
+
+        if(tmp->key == k){
+            tmp->value= v;
+        }else{
+            tmp= tmp->siguiente;
+        }
+        c++;
+    }
+    delete tmp;
+    cout << "assocM" << showMap(m);
+}
+
+//lookupM :: k -> Map k v -> Maybe v O(log K)
+
+String lookupM  (String k, Map m){
+    int c = 0;
+    NodoS* tmp = map->primero;
+
+    while(c < m->cantidad){
+
+        if(tmp->key == k){
+            cout << "lookupM" << showMap(m) << tmp->value;
+            return tmp->value;
+        }else{
+            tmp= tmp->siguiente;
+        }
+        c++;
+    }   
+    delete tmp;
+    return NULL;
+}
+
+
+//deleteM :: k -> Map k v -> Map k v O(log K)
+void deleteM (String k, Map m){
+    cout << "before deleteM" << showMap(m);
+    int c = 0;
+    NodoS* tmp = map->primero;
+
+    while(c < m->cantidad){
+
+        if(tmp->key == k){
+           tmp= tmp->siguiente;
+        }else{
+            tmp= tmp->siguiente;
+        }
+        c++;
+    }   
+    delete tmp;
+    cout << "aafter deleteM" << showMap(m);
+}
+
+
+//domM :: Map k v -> [k] O(K)
+
+String domM (Map m){
+    String[m->cantidad] keysTemp;
+
+    NodoS* tmp = map->primero;
+
+    while(c < m->cantidad){
+        keysTemp= tmp->key;
+        tmp= tmp->siguiente;
+        c++;
+    } 
+    delete tmp;
+}
+
+
+void showMap (Map m){
+    NodoS* tmp = m->primero;
+    int i = 0;
+    while(i < m->cantidad){
+        cout << tmp->key  << " - " << tmp->value << " | " ;
+        tmp = tmp->siguiente;
+    }
+}
--- tp-8/Nave/c++/Map.h
+++ tp-8/Nave/c++/Map.h
@@ -22,96 +22,14 @@ struct MapSt {
 typedef Map *MapSt;
 
 
-//emptyM :: Map k v O(1) --haskell
-//costo: constante O(1) 
-Map emptyM () {
-    MapSt* m = new MapSt;
-    m->cantidad=0;
-    m->primero=NULL;
-    return m;
-};
+Map emptyM (); 
 
-//assocM :: k -> v -> Map k v -> Map k v O(log K) --haskell
-//costo: O (log K) donde K es la cantidad de las claves en el map - esta bien?
-void assocM (String k, String v, Map m){
-    int c = 0;
-    NodoS* tmp = map->primero;
+void assocM (String k, String v, Map m);
 
-    while(c < m->cantidad){
+Maybe lookupM(String k, Map m);
 
-        if(tmp->key == k){
-            tmp->value= v;
-        }else{
-            tmp= tmp->siguiente;
-        }
-        c++;
-    }
-    delete tmp;
-    cout << "assocM" << showMap(m);
-}
+void deleteM (String k, Map m); 
 
-//lookupM :: k -> Map k v -> Maybe v O(log K)
+String domM(Map m);
 
-String lookupM  (String k, Map m){
-    int c = 0;
-    NodoS* tmp = map->primero;
-
-    while(c < m->cantidad){
-
-        if(tmp->key == k){
-            cout << "lookupM" << showMap(m) << tmp->value;
-            return tmp->value;
-        }else{
-            tmp= tmp->siguiente;
-        }
-        c++;
-    }   
-    delete tmp;
-    return NULL;
-}
-
-
-//deleteM :: k -> Map k v -> Map k v O(log K)
-void deleteM (String k, Map m){
-    cout << "before deleteM" << showMap(m);
-    int c = 0;
-    NodoS* tmp = map->primero;
-
-    while(c < m->cantidad){
-
-        if(tmp->key == k){
-           tmp= tmp->siguiente;
-        }else{
-            tmp= tmp->siguiente;
-        }
-        c++;
-    }   
-    delete tmp;
-    cout << "aafter deleteM" << showMap(m);
-}
-
-
-//domM :: Map k v -> [k] O(K)
-
-string domM (Map m){
-    string[m->cantidad] keysTemp;
-
-    NodoS* tmp = map->primero;
-
-    while(c < m->cantidad){
-        keysTemp= tmp->key;
-        tmp= tmp->siguiente;
-        c++;
-    } 
-    delete tmp;
-}
-
-
-void showMap (Map m){
-    NodoS* tmp = m->primero;
-    int i = 0;
-    while(i < m->cantidad){
-        cout << tmp->key  << " - " << tmp->value << " | " ;
-        tmp = tmp->siguiente;
-    }
-}
+void showMap(Map m);
