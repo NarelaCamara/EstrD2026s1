2894fe124d6145b4146edaf7b6b9ff9e9f3382c3
NarelaCamara <roxana.camara07@gmail.com>
2026-07-05 04:13:26 -0300

    feat: mago

---
+++ tp-8/Magos/cplusplus/tp-8.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "map/Map.h"
+#include "maxHeap/MaxHeap.h"
+#include "set/Set.h"
+#include "mago/Mago.h"
+
+
+int main() {
+    testMap();
+    testMaxHeap();
+    testSet();
+    testMago();
+    return 0;
+}
